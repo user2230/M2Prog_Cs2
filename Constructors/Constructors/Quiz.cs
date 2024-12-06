df65f4d76883380a01d10d37048e871961fe7f6e
user2230 <furkanefeceylan2008@hotmail.com>
2024-12-06 10:56:21 +0100

    commit

--- Constructors/Constructors/Quiz.cs
+++ Constructors/Constructors/Quiz.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Constructors
 {
-   
+
     internal class Quiz
     {
         internal QuizVraag[] vragen;
         internal QuizVraagAntwoord[] ingevuldeAntwoorden;
-       
+
         internal Quiz(int aantalVragen)
         {
             vragen = new QuizVraag[aantalVragen];
@@ -23,7 +23,15 @@ namespace Constructors
 
         internal void VoegVraagToeOpIndex(int index, string vraag, string antwoord)
         {
-           vragen[index] = vraag;
+            QuizVraag quizVraag = new QuizVraag(vraag, antwoord);
+            VoegVraagToeOpIndex(index, quizVraag);
         }
+
+        internal void StelVraag(int index);
+        {
+            Quizvraag vraag = QuizVraag[index];
+            QuizVraagAntwoord
+            
+
     }
 }
