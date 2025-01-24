f10b28d2070169ead368771e2dbd990777f8b262
user2230 <furkanefeceylan2008@hotmail.com>
2025-01-24 10:58:14 +0100

    git

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
+
+        static void Main()
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            personeelOpNummer.Add(469024, "pim");
+            personeelOpNummer.Add(749145, "pam");
+
+            string pim = personeelOpNummer[469024];
+            string pam = personeelOpNummer[749145];
+
+            Console.WriteLine(pim + "\n" + pam);
+
+            foreach (KeyValuePair<int, string> item in personeelOpNummer)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+
+        }
+    }
+}
